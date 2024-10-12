7ca81d2a2db71fdf86afca60b844953f42d61f3e
aakashnarla <aakash.narla.rao@gmail.com>
2024-10-12 15:39:36 +0530

    Few Cleanup.

--- src/main/java/com/aakashnarla/playground/GreetingResource.java
+++ src/main/java/com/aakashnarla/playground/GreetingResource.java
@@ -5,33 +5,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("")
+@Path("/hello")
 public class GreetingResource {
 
     @GET
-    @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
-    }
-    @GET
-    @Path("/api/greeting")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Greeting getGreeting() {
-        return new Greeting("Hello from Quarkus!");
-    }
-
-    public static class Greeting {
-        private String message;
-
-        public Greeting() { }
-
-        public Greeting(String message) {
-            this.message = message;
-        }
-
-        public String getMessage() { return message; }
-
-        public void setMessage(String message) { this.message = message; }
     }
 }
